a4a8cc3d21481d9ef95ccb2b9f8d5c650e7d0d90
soulcure <colin.soulcure@gmail.com>
2021-02-17 00:36:53 +0800

    gb2312 to utf-8

--- src/app/conn_server/conn_service.cc
+++ src/app/conn_server/conn_service.cc
@@ -19,23 +19,25 @@ static int sec_send_pkt;
 int total_send_pkts;
 int total_send_pkt;
 
-#define CONFIG_FDSET_INCR  128
-#define CONF_CONN_LISTEN_IP  "listen_ip"
+#define CONFIG_FDSET_INCR 128
+#define CONF_CONN_LISTEN_IP "listen_ip"
 #define CONF_CONN_LISTEN_PORT "listen_port"
-ConnService::ConnService() :loop_(getEventLoop()), tcpService_(this, loop_),m_msgService(loop_,this) {
+ConnService::ConnService() : loop_(getEventLoop()), tcpService_(this, loop_), m_msgService(loop_, this)
+{
 	m_max_conn = maxclients + CONFIG_FDSET_INCR;
 	loop_->init(m_max_conn);
-	m_clients = new Client[sizeof(Client)*(m_max_conn + 1)];
+	m_clients = new Client[sizeof(Client) * (m_max_conn + 1)];
 	m_conns = 0;
 	m_total_pkts = 0;
-	m_total_chat_pkts=0;
+	m_total_chat_pkts = 0;
 }
 
 int ConnService::init()
 {
 	m_ip = ConfigFileReader::getInstance()->ReadString(CONF_CONN_LISTEN_IP);
 	m_port = ConfigFileReader::getInstance()->ReadInt(CONF_CONN_LISTEN_PORT);
-	if (m_ip == "" || m_port == 0) {
+	if (m_ip == "" || m_port == 0)
+	{
 		LOGE("not config out ip or port");
 		return -1;
 	}
@@ -43,13 +45,16 @@ int ConnService::init()
 	return m_msgService.init();
 }
 
-int ConnService::start() {
+int ConnService::start()
+{
 	LOGD("connect server listen on %s:%d", m_ip.c_str(), m_port);
-	if (tcpService_.listen(m_ip, m_port) == -1) {
+	if (tcpService_.listen(m_ip, m_port) == -1)
+	{
 		LOGE("listen [ip:%s,port:%d]fail", m_ip.c_str(), m_port);
 		return -1;
 	}
-	if (m_msgService.start() == -1) {
+	if (m_msgService.start() == -1)
+	{
 		return -1;
 	}
 	tcpService_.run();
@@ -61,33 +66,37 @@ int ConnService::getNodeId()
 	return ConfigFileReader::getInstance()->ReadInt(CONF_NODE_ID);
 }
 
-
-int ConnService::recvBusiMsg(int sockfd, PDUBase & _data)
+int ConnService::recvBusiMsg(int sockfd, PDUBase &_data)
 {
-	if (sockfd > m_max_conn) {
+	if (sockfd > m_max_conn)
+	{
 		return -1;
 	}
 	std::string user_id(_data.terminal_token, sizeof(_data.terminal_token));
-	Client* client = &m_clients[sockfd];
-	if (strcmp(client->user_id.c_str(), user_id.c_str())) {
-		LOGE("sockfd[%d] is not the user[%s],belong to(%s)", sockfd, user_id.c_str(),client->user_id.c_str());
+	Client *client = &m_clients[sockfd];
+	if (strcmp(client->user_id.c_str(), user_id.c_str()))
+	{
+		LOGE("sockfd[%d] is not the user[%s],belong to(%s)", sockfd, user_id.c_str(), client->user_id.c_str());
 		return -1;
-
 	}
 	int cmd = _data.command_id;
-	
-	if (tcpService_.Send(sockfd, _data) == -1) {
+
+	if (tcpService_.Send(sockfd, _data) == -1)
+	{
 		LOGE("send to user[%s] fail", user_id.c_str());
 	}
 	++total_send_pkts;
-	if (cmd == USER_LOGIN_ACK) {
+	if (cmd == USER_LOGIN_ACK)
+	{
 		User_Login_Ack ack;
-		if (!ack.ParseFromArray(_data.body.get(), _data.length)) {
-			LOGERROR(_data.command_id, _data.seq_id, "User_Login_Ack°ü½âÎö´íÎó");
+		if (!ack.ParseFromArray(_data.body.get(), _data.length))
+		{
+			LOGERROR(_data.command_id, _data.seq_id, "User_Login_AckåŒ…è§£æžé”™è¯¯");
 			return 0;
 		}
-		if (ack.errer_no() == ERRNO_CODE_OK) {
-			Client* client = &m_clients[sockfd];
+		if (ack.errer_no() == ERRNO_CODE_OK)
+		{
+			Client *client = &m_clients[sockfd];
 			client->state = CLIENT_ONLINE;
 			//TODO  couldn't close user socket
 		}
@@ -96,21 +105,26 @@ int ConnService::recvBusiMsg(int sockfd, PDUBase & _data)
 	return 0;
 }
 
-void ConnService::onData(int _sockfd, PDUBase* _base) {
-	if (_sockfd > m_max_conn) {
+void ConnService::onData(int _sockfd, PDUBase *_base)
+{
+	if (_sockfd > m_max_conn)
+	{
 		LOGW("too much connection");
 		tcpService_.closeSocket(_sockfd);
 		return;
 	}
-	Client* client = &m_clients[_sockfd];
-	std::string user_id=std::string(_base->terminal_token, sizeof(_base->terminal_token));
-	if (user_id[0] == 0) {
+	Client *client = &m_clients[_sockfd];
+	std::string user_id = std::string(_base->terminal_token, sizeof(_base->terminal_token));
+	if (user_id[0] == 0)
+	{
 		LOGE(" msg header not include user_id");
 		delete _base;
 		return;
 	}
-	if (client->state == CLIENT_OFFLINE && _base->command_id!=USER_LOGIN) {
-		if (get_mstime() - client->t > 5) {
+	if (client->state == CLIENT_OFFLINE && _base->command_id != USER_LOGIN)
+	{
+		if (get_mstime() - client->t > 5)
+		{
 			LOGE("user(%s) offline", user_id.c_str());
 			User_Offline offline;
 			_base->ResetPackBody(offline, USER_LOGOFF);
@@ -121,7 +135,7 @@ void ConnService::onData(int _sockfd, PDUBase* _base) {
 	}
 	client->user_id = user_id;
 	//client->app_id = std::string(_base->app_id, sizeof(_base->app_id));
-	LOGD("recv user(%s) msg cmd(%d)", client->user_id.c_str(),_base->command_id);
+	LOGD("recv user(%s) msg cmd(%d)", client->user_id.c_str(), _base->command_id);
 	m_msgService.recvClientMsg(_sockfd, *_base);
 	++total_recv_pkt;
 	delete _base;
@@ -129,42 +143,49 @@ void ConnService::onData(int _sockfd, PDUBase* _base) {
 
 void ConnService::onEvent(int fd, ConnectionEvent event)
 {
-	if (event == Connected) {
+	if (event == Connected)
+	{
 		onConnnect(fd);
 	}
-	else {
+	else
+	{
 		onDisconnnect(fd);
 	}
 }
 
-void ConnService::onConnnect(int _sockfd) {
-	if (_sockfd > m_max_conn) {
+void ConnService::onConnnect(int _sockfd)
+{
+	if (_sockfd > m_max_conn)
+	{
 		LOGW("too much connection");
 		tcpService_.closeSocket(_sockfd);
 		return;
 	}
-   // LOGD("sockfd:%d connect",_sockfd);
-	Client* client = &m_clients[_sockfd];
+	// LOGD("sockfd:%d connect",_sockfd);
+	Client *client = &m_clients[_sockfd];
 	client->t = get_mstime();
 	m_conns++;
 }
 
-void ConnService::onDisconnnect(int _sockfd) {
-	if (_sockfd > m_max_conn) {
+void ConnService::onDisconnnect(int _sockfd)
+{
+	if (_sockfd > m_max_conn)
+	{
 		return;
 	}
-	Client* client = &m_clients[_sockfd];
+	Client *client = &m_clients[_sockfd];
 	client->t = 0;
 	--m_conns;
 	tcpService_.closeSocket(_sockfd);
-	if (client->state == CLIENT_OFFLINE) {
+	if (client->state == CLIENT_OFFLINE)
+	{
 		return;
 	}
 	client->state = CLIENT_OFFLINE;
 	LOGT("user[userid:%s,fd:%d] disconnect", client->user_id.c_str(), _sockfd);
 	User_LogOff log_off;
 	log_off.set_user_id(client->user_id);
-	
+
 	PDUBase pack;
 	std::shared_ptr<char> body(new char[log_off.ByteSize()], carray_deleter);
 	log_off.SerializeToArray(body.get(), log_off.ByteSize());
@@ -175,39 +196,44 @@ void ConnService::onDisconnnect(int _sockfd) {
 	pack.seq_id = 0;
 	memcpy(pack.terminal_token, client->user_id.c_str(), sizeof(pack.terminal_token));
 	m_msgService.recvClientMsg(_sockfd, pack);
-	
+
 	//deleteClient(client->user_id);
 }
 
-void ConnService::parse(Connection* conn) {
+void ConnService::parse(Connection *conn)
+{
 	int fd = conn->fd;
-	char* data = conn->buf;
-	while (conn->buf_len >= HEAD_LEN) {
-		//Ã»ÓÐ²»ÍêÕûµÄ°ü
-		if (!conn->less_pkt_len) {
-			int* startflag = (int*)(data);
-			if (ntohl(*(startflag)) == PDUBase::startflag)//Õý³£Çé¿öÏÂ ÏÈ½ÓÊÕµ½°üÍ·
+	char *data = conn->buf;
+	while (conn->buf_len >= HEAD_LEN)
+	{
+		//æ²¡æœ‰ä¸å®Œæ•´çš„åŒ…
+		if (!conn->less_pkt_len)
+		{
+			int *startflag = (int *)(data);
+			if (ntohl(*(startflag)) == PDUBase::startflag) //æ­£å¸¸æƒ…å†µä¸‹ å…ˆæŽ¥æ”¶åˆ°åŒ…å¤´
 			{
-				if (conn->buf_len >= HEAD_LEN) {
-					int data_len = ntohl(*(int*)(data + 53));
+				if (conn->buf_len >= HEAD_LEN)
+				{
+					int data_len = ntohl(*(int *)(data + 53));
 					int pkt_len = data_len + HEAD_LEN;
 					//	msg_t msg;
-					PDUBase* pdu = new PDUBase;
+					PDUBase *pdu = new PDUBase;
 
 					int len = conn->buf_len >= pkt_len ? pkt_len : conn->buf_len;
 					pdu->_OnPduParse(data, len);
-					if (conn->buf_len >= pkt_len) {
+					if (conn->buf_len >= pkt_len)
+					{
 
 						conn->buf_len -= pkt_len;
 						conn->recv_pkt++;
 						data += pkt_len;
 						//	msg_info(msg);
 						onData(fd, pdu);
-						
 					}
 					//not enough a pkt ;
-					else {
-						conn->pdu = pdu;//¼ÇÂ¼²»ÍêÕûpdu
+					else
+					{
+						conn->pdu = pdu; //è®°å½•ä¸å®Œæ•´pdu
 						conn->pkt_len = pkt_len;
 						conn->less_pkt_len = pkt_len - conn->buf_len;
 						conn->buf_len = 0;
@@ -215,43 +241,49 @@ void ConnService::parse(Connection* conn) {
 					}
 				}
 			}
-			else {//ÕÒµ½°üÍ·
+			else
+			{ //æ‰¾åˆ°åŒ…å¤´
 				LOGW("err data");
 				int i;
-				for (i = 0; i < conn->buf_len - 3; ++i) {
-					if (ntohl(*((int*)(data + i))) == SPDUBase::startflag) {
+				for (i = 0; i < conn->buf_len - 3; ++i)
+				{
+					if (ntohl(*((int *)(data + i))) == SPDUBase::startflag)
+					{
 						data += i;
 						conn->buf_len -= i;
 						break;
 					}
 				}
-				if (i == conn->buf_len - 3) {
+				if (i == conn->buf_len - 3)
+				{
 					conn->clear();
 					break;
 				}
 			}
 		}
-		else {
-			//´¦Àí²»ÍêÕûpdu
+		else
+		{
+			//å¤„ç†ä¸å®Œæ•´pdu
 			int less_pkt_len = conn->less_pkt_len;
-			PDUBase* pdu = conn->pdu;
-			char* pData = pdu->body.get();
+			PDUBase *pdu = conn->pdu;
+			char *pData = pdu->body.get();
 			int len = conn->buf_len >= less_pkt_len ? less_pkt_len : conn->buf_len;
 			memcpy(pData + (conn->pkt_len - conn->less_pkt_len - HEAD_LEN), data, len);
-			if (conn->buf_len >= less_pkt_len) {
+			if (conn->buf_len >= less_pkt_len)
+			{
 
 				data += less_pkt_len;
 				conn->buf_len -= less_pkt_len;
-				//Çå¿Õ¼ÇÂ¼
+				//æ¸…ç©ºè®°å½•
 				conn->pdu = NULL;
 				conn->pkt_len = 0;
 				conn->less_pkt_len = 0;
 				conn->recv_pkt++;
 				//msg_info(msg);
-				onData(fd, pdu);	
-				
+				onData(fd, pdu);
 			}
-			else {
+			else
+			{
 				conn->less_pkt_len -= conn->buf_len;
 				conn->buf_len = 0;
 			}
@@ -259,7 +291,8 @@ void ConnService::parse(Connection* conn) {
 	}
 
 	//move not enough header data to buf start
-	if (conn->buf_len && data != conn->buf) {
+	if (conn->buf_len && data != conn->buf)
+	{
 		memmove(conn->buf, data, conn->buf_len);
 	}
 }
@@ -268,10 +301,11 @@ int ConnService::deleteClient(int _user_id)
 {
 }
 
-void ConnService::Timer(int fd, short mask, void * privdata)
+void ConnService::Timer(int fd, short mask, void *privdata)
 {
-	ConnService* server = reinterpret_cast<ConnService*>(privdata);
-	if (!server) {
+	ConnService *server = reinterpret_cast<ConnService *>(privdata);
+	if (!server)
+	{
 		return;
 	}
 	server->reportOnliners();
@@ -282,14 +316,14 @@ void ConnService::reportOnliners()
 {
 	static uint64_t m_cronloops = 0;
 	static uint64_t last_statistics_pkts = 0;
-	
+
 	Report_onliners req;
 	req.set_onliners(m_conns);
 	SPDUBase spdu;
 	spdu.ResetPackBody(req, REPORT_ONLINERS);
 	NodeMgr::getInstance()->sendSid(SID_LOADBALANCE, spdu);
 
-//	printf("total recv %d pkts,every recv %d pkt,  total send:%d ,every sec send:%d \n",  total_recv_pkt, total_recv_pkt - sec_recv_pkt, total_send_pkts, total_send_pkts-sec_send_pkt);
+	//	printf("total recv %d pkts,every recv %d pkt,  total send:%d ,every sec send:%d \n",  total_recv_pkt, total_recv_pkt - sec_recv_pkt, total_send_pkts, total_send_pkts-sec_send_pkt);
 	sec_recv_pkt = total_recv_pkt;
 	sec_send_pkt = total_send_pkts;
 }
@@ -304,10 +338,10 @@ void ConnService::count(int cmd)
 		++it->second;
 	}*/
 	++m_total_pkts;
-	if (cmd == CID_CHAT_BUDDY || cmd == CID_CHAT_CUSTOMER_SERVICES || cmd == CID_CHAT_GROUP || cmd == CID_CHAT_MACHINE) {
+	if (cmd == CID_CHAT_BUDDY || cmd == CID_CHAT_CUSTOMER_SERVICES || cmd == CID_CHAT_GROUP || cmd == CID_CHAT_MACHINE)
+	{
 		++m_total_chat_pkts;
 	}
-	
 }
 
 //statistic every 10 second
@@ -316,10 +350,11 @@ void ConnService::statistic()
 	static long long loop = 0;
 	static long long last_chat_msg_pkts = 0;
 	static long long last_total_pkts = 0;
-	if (loop & 1) {
+	if (loop & 1)
+	{
 		//LOGI("recv msg rate -------chat msg:%d pkts/10sec----------- total msg:%d pkts/10sec", m_total_chat_pkts - last_chat_msg_pkts, m_total_pkts - last_total_pkts);
 		last_chat_msg_pkts = m_total_chat_pkts;
 		last_total_pkts = m_total_pkts;
 	}
-    ++loop;
+	++loop;
 }

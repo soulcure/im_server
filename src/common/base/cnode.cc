290e5bebcd4f28bbc8031c5a1ea5326d8e8fc9ce
soulcure <colin.soulcure@gmail.com>
2021-02-18 16:40:28 +0800

    gb2312 to utf-8

--- src/common/base/cnode.cc
+++ src/common/base/cnode.cc
@@ -20,42 +20,45 @@ CNode::~CNode()
 {
 }
 
-CNode * CNode::getInstance()
+CNode *CNode::getInstance()
 {
 	static CNode node;
 	return &node;
 }
 
-bool CNode::handler(int sockfd, SPDUBase& base)
+bool CNode::handler(int sockfd, SPDUBase &base)
 {
 	int cid = base.command_id;
-	switch (cid) {
+	switch (cid)
+	{
 	case CID_S2S_AUTHENTICATION_REQ:
 		authenticationReq(sockfd, base);
 		break;
 	case CID_S2S_PING:
 		pingReq(sockfd, base);
 		break;
-	default://other msg handler by application
+	default: //other msg handler by application
 		LOGD("unknow cmd(%d)", cid);
 		break;
 	}
 	return false;
 }
 
-bool CNode::authenticationReq(int sockfd, SPDUBase& base)
+bool CNode::authenticationReq(int sockfd, SPDUBase &base)
 {
 	com::proto::server::AuthenticationReq req;
-	if (!req.ParseFromArray(base.body.get(), base.length)) {
-		LOGERROR(base.command_id, base.seq_id, "authenticationReq°ü½âÎö´íÎó");
+	if (!req.ParseFromArray(base.body.get(), base.length))
+	{
+		LOGERROR(base.command_id, base.seq_id, "authenticationReqåŒ…è§£æžé”™è¯¯");
 		return -1;
 	}
 	uint16_t sid = req.service_type();
 	uint32_t nid = req.node_id();
 	LOGD("recv node[sid:%s,nid:%d] authenticationReq", sidName(sid), nid);
 	//only  after recving broadcast,we can consider the node able;
-	Node* node = findNode(sid, nid);
-	if (!node) {
+	Node *node = findNode(sid, nid);
+	if (!node)
+	{
 		return false;
 	}
 	node->fd = sockfd;
@@ -71,7 +74,8 @@ bool CNode::authenticationReq(int sockfd, SPDUBase& base)
 	}*/
 	int cur_sid = ConfigFileReader::getInstance()->ReadInt(CONF_SERVICE_TYPE);
 	int cur_nid = ConfigFileReader::getInstance()->ReadInt(CONF_NODE_ID);
-	if (cur_nid == 0 | cur_sid == 0) {
+	if (cur_nid == 0 | cur_sid == 0)
+	{
 		LOGD("node[sid:%s,nid:%d] authenticationReq fail", sidName(sid), nid);
 		return false;
 	}
@@ -87,11 +91,12 @@ bool CNode::authenticationReq(int sockfd, SPDUBase& base)
 	return true;
 }
 
-bool CNode::pingReq(int sockfd, SPDUBase& base)
+bool CNode::pingReq(int sockfd, SPDUBase &base)
 {
 	PingReq req;
-	if (!req.ParseFromArray(base.body.get(), base.length)) {
-		LOGERROR(base.command_id, base.seq_id, "authenticationReq°ü½âÎö´íÎó");
+	if (!req.ParseFromArray(base.body.get(), base.length))
+	{
+		LOGERROR(base.command_id, base.seq_id, "authenticationReqåŒ…è§£æžé”™è¯¯");
 		return false;
 	}
 	int sid = req.service_type();
@@ -107,19 +112,22 @@ bool CNode::pingReq(int sockfd, SPDUBase& base)
 	return true;
 }
 
-int CNode::pasreRouteSlotStr(const char * str, int len, Node * node)
+int CNode::pasreRouteSlotStr(const char *str, int len, Node *node)
 {
-	char* tmp = (char*)malloc(len + 1);
-	if (!tmp) {
+	char *tmp = (char *)malloc(len + 1);
+	if (!tmp)
+	{
 		return -1;
 	}
 	memset(tmp, 0, len + 1);
 	strcpy(tmp, str);
-	char* p = tmp;
-	char* pos = tmp;
+	char *p = tmp;
+	char *pos = tmp;
 	int segment = 0;
-	while (*pos) {
-		if (*pos != ' ') {
+	while (*pos)
+	{
+		if (*pos != ' ')
+		{
 			*p++ = *pos;
 		}
 		pos++;
@@ -128,37 +136,47 @@ int CNode::pasreRouteSlotStr(const char * str, int len, Node * node)
 	memset(node->slots, 0, sizeof(node->slots));
 	//printf("str:%s\n", tmp);
 	pos = tmp;
-	char* begin = NULL;;
-	char* end = NULL;;
+	char *begin = NULL;
+	;
+	char *end = NULL;
+	;
 	int right;
 	int left;
 	int flag = 0;
 	int res = 0;
-	while (*pos) {
-		if (*pos == ',') {
+	while (*pos)
+	{
+		if (*pos == ',')
+		{
 			pos++;
 		}
-		if (*pos == '[') {
+		if (*pos == '[')
+		{
 			begin = pos + 1;
-			while (*pos) {
-				if (*pos == '.') {
+			while (*pos)
+			{
+				if (*pos == '.')
+				{
 					*pos = '\0';
 					left = atoi(begin);
-					if (*(pos + 1) != '.' || *(pos + 2) != '.') {
+					if (*(pos + 1) != '.' || *(pos + 2) != '.')
+					{
 						LOGE("format error");
 						goto end;
 					}
 					pos += 3;
 					begin = pos;
-					if (!(*pos >= '1'  && *pos <= '9')) {
+					if (!(*pos >= '1' && *pos <= '9'))
+					{
 						LOGE("format error");
 						goto end;
 					}
 					flag = 1;
-
 				}
-				if (*pos == ']') {
-					if (!flag) {
+				if (*pos == ']')
+				{
+					if (!flag)
+					{
 						LOGE("format error");
 						goto end;
 					}
@@ -166,48 +184,57 @@ int CNode::pasreRouteSlotStr(const char * str, int len, Node * node)
 					end = pos;
 					*pos = '\0';
 					right = atoi(begin);
-					pos++;//skip ]
+					pos++; //skip ]
 					break;
 				}
 				pos++;
 			}
-			if (!end) {
+			if (!end)
+			{
 				LOGE("not find ] ");
 				goto end;
 			}
-			if (right<left) {
+			if (right < left)
+			{
 				LOGE("right lower left");
 				goto end;
 			}
 			end = NULL;
-			if (right>255) {
+			if (right > 255)
+			{
 				LOGE("slot should low 255");
 				goto end;
 			}
 			//	printf("left:%d,right:%d\n", left, right);
-			for (int i = left; i <= right; i++) {
+			for (int i = left; i <= right; i++)
+			{
 				int index = i / 8;
 				int bit = i % 8;
 				node->slots[index] |= 1 << bit;
 			}
 			continue; //skip tail pos++
 		}
-		else {
+		else
+		{
 			begin = pos;
-			if (!(*begin >= '1'  && *begin <= '9')) {
+			if (!(*begin >= '1' && *begin <= '9'))
+			{
 				return -1;
 			}
-			while (*pos) {
-				if (*pos == ',') {
+			while (*pos)
+			{
+				if (*pos == ',')
+				{
 					*pos = '\0';
 					left = atoi(begin);
-					pos++;//skip ,
+					pos++; //skip ,
 					break;
 				}
 				pos++;
 			}
 			//only one item
-			if (!*pos) {
+			if (!*pos)
+			{
 				left = atoi(begin);
 			}
 			//printf("signal:%d\n", left);
@@ -226,55 +253,62 @@ end:
 	return -1;
 }
 
-int CNode::init(TcpService * net)
+int CNode::init(TcpService *net)
 {
 	m_pNet = net;
 	return 0;
 }
 
-int CNode::configureState(SPDUBase & base)
+int CNode::configureState(SPDUBase &base)
 {
 	ServerInfoBroadcast broadcast;
 
-	if (!broadcast.ParseFromArray(base.body.get(), base.length)) {
-		LOGERROR(base.command_id, base.seq_id, "ServerInfoBroadcast°ü½âÎö´íÎó");
+	if (!broadcast.ParseFromArray(base.body.get(), base.length))
+	{
+		LOGERROR(base.command_id, base.seq_id, "ServerInfoBroadcaståŒ…è§£æžé”™è¯¯");
 		return -1;
 	}
 
-	const auto& server_list = broadcast.service_list();
+	const auto &server_list = broadcast.service_list();
 	for (int i = 0; i < broadcast.service_list_size(); i++)
 	{
-		auto& srv_info = broadcast.service_list(i);
+		auto &srv_info = broadcast.service_list(i);
 		int type = srv_info.service_type();
 
 		for (int i = 0; i < srv_info.node_info_list_size(); i++)
 		{
-			auto& node_info = srv_info.node_info_list(i);
+			auto &node_info = srv_info.node_info_list(i);
 			uint32_t node_id = node_info.node_id();
-			if (node_info.is_enable())//×¢²á
+			if (node_info.is_enable()) //æ³¨å†Œ
 			{
-				Node* node = addNode(type, node_id);
-				if (!node) {
+				Node *node = addNode(type, node_id);
+				if (!node)
+				{
 					LOGE("add node[sid:%d,nid:%d] fail", sidName(type), node_id);
 					continue;
 				}
 				int stragry = node_info.route_strategy();
 				node->stragry = stragry;
-				if (stragry == ROUTE_USER) {
+				if (stragry == ROUTE_USER)
+				{
 					LOGD("node[sid:%s,nid:%d] route to user", sidName(type), node_id);
 					node->user_list.clear();
-					const auto& user_list = node_info.user_list();
-					for (int i = 0; i < user_list.size(); i++) {
+					const auto &user_list = node_info.user_list();
+					for (int i = 0; i < user_list.size(); i++)
+					{
 						node->user_list.push_back(user_list[i]);
 					}
 				}
-				else if (stragry == ROUTE_SLOT) {
-					if (pasreRouteSlotStr(node_info.slots().c_str(), strlen(node_info.slots().c_str()), node) == -1) {
+				else if (stragry == ROUTE_SLOT)
+				{
+					if (pasreRouteSlotStr(node_info.slots().c_str(), strlen(node_info.slots().c_str()), node) == -1)
+					{
 						LOGE("parse slot str fail");
 					}
 				}
 			}
-			else {
+			else
+			{
 				delNode(type, node_id);
 			}
 		}
@@ -283,11 +317,11 @@ int CNode::configureState(SPDUBase & base)
 	return true;
 }
 
-
 bool CNode::existNode(int sid, int nid)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
-	if (findNode(sid, nid)) {
+	if (findNode(sid, nid))
+	{
 		return true;
 	}
 	return false;
@@ -296,8 +330,9 @@ bool CNode::existNode(int sid, int nid)
 int CNode::setNodeConnect(int sid, int nid, int sockfd)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
-	Node* node;
-	if ((node=findNode(sid, nid))!=NULL) {
+	Node *node;
+	if ((node = findNode(sid, nid)) != NULL)
+	{
 		node->fd = sockfd;
 		node->connected = true;
 		return 0;
@@ -305,15 +340,18 @@ int CNode::setNodeConnect(int sid, int nid, int sockfd)
 	return -1;
 }
 
-CNode::Node * CNode::findNode(int sid, int nid)
+CNode::Node *CNode::findNode(int sid, int nid)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
 	auto sit = m_services.begin();
-	while (sit != m_services.end()) {
-		auto& node_list = sit->second;
-		for (auto nit = node_list.begin(); nit != node_list.end(); nit++) {
-			Node* node = *nit;
-			if (node->sid == sid && node->nid == nid) {
+	while (sit != m_services.end())
+	{
+		auto &node_list = sit->second;
+		for (auto nit = node_list.begin(); nit != node_list.end(); nit++)
+		{
+			Node *node = *nit;
+			if (node->sid == sid && node->nid == nid)
+			{
 				return node;
 			}
 		}
@@ -326,11 +364,14 @@ int CNode::setNodeDisconnect(int sockfd)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
 	auto sit = m_services.begin();
-	while (sit != m_services.end()) {
-		auto& node_list = sit->second;
-		for (auto nit = node_list.begin(); nit != node_list.end(); nit++) {
-			Node* node = *nit;
-			if (node->fd == sockfd) {
+	while (sit != m_services.end())
+	{
+		auto &node_list = sit->second;
+		for (auto nit = node_list.begin(); nit != node_list.end(); nit++)
+		{
+			Node *node = *nit;
+			if (node->fd == sockfd)
+			{
 				node->connected = false;
 				node->fd = -1;
 				return 0;
@@ -341,41 +382,50 @@ int CNode::setNodeDisconnect(int sockfd)
 	return -1;
 }
 
-CNode::Node* CNode::getAccessNode(int sid, const std::string& user_id)
+CNode::Node *CNode::getAccessNode(int sid, const std::string &user_id)
 {
 	int unable = 0;
 	auto sit = m_services.find(sid);
-	if (sit != m_services.end()) {
-		auto& node_list = sit->second;
-		for (auto nit = node_list.begin(); nit != node_list.end(); ) {
-			Node* node = *nit;
-			if (node->unable) {
-				nit =node_list.erase(nit);
+	if (sit != m_services.end())
+	{
+		auto &node_list = sit->second;
+		for (auto nit = node_list.begin(); nit != node_list.end();)
+		{
+			Node *node = *nit;
+			if (node->unable)
+			{
+				nit = node_list.erase(nit);
 				delete node;
 				continue;
 			}
-			if (!node->connected) {
+			if (!node->connected)
+			{
 				++unable;
 				nit++;
 				continue;
 			}
-			if (node->stragry == ROUTE_USER) {
+			if (node->stragry == ROUTE_USER)
+			{
 				++unable;
-				for (auto uit = node->user_list.begin(); uit != node->user_list.end(); uit++) {
-					if (*uit == user_id) {
-						LOGD("user[uid:%s] route to node[sid:%s nid:%d] according to ROUTE_USER", user_id.c_str(), sidName(sid),node->nid);
+				for (auto uit = node->user_list.begin(); uit != node->user_list.end(); uit++)
+				{
+					if (*uit == user_id)
+					{
+						LOGD("user[uid:%s] route to node[sid:%s nid:%d] according to ROUTE_USER", user_id.c_str(), sidName(sid), node->nid);
 						return node;
 					}
 				}
 			}
 			nit++;
 		}
-		const char* p = user_id.c_str();
-		if (sid == SID_MSG) {
+		const char *p = user_id.c_str();
+		if (sid == SID_MSG)
+		{
 			//slot
-			int slot = dictGenHashFunction((const unsigned char*)p, user_id.length()) % CLUSTER_SLOTS;
-			Node* node = msg_slots[slot];
-			if (node) {
+			int slot = dictGenHashFunction((const unsigned char *)p, user_id.length()) % CLUSTER_SLOTS;
+			Node *node = msg_slots[slot];
+			if (node)
+			{
 				//LOGD("user[uid:%s] route to node[sid:%s,nid:%d] according to ROUTE_SLOTS", user_id.c_str(), sidName(sid),node->nid);
 				return node;
 			}
@@ -383,16 +433,19 @@ CNode::Node* CNode::getAccessNode(int sid, const std::string& user_id)
 			return NULL;
 		}
 
-		
 		int able = node_list.size() - unable;
-		if (able && able > 0) {
-			int index = dictGenHashFunction((const unsigned char*)&user_id, sizeof(int)) % able;
-			for (auto nit = node_list.begin(); nit != node_list.end(); nit++) {
-				Node* node = *nit;
-				if (!node->connected) {
+		if (able && able > 0)
+		{
+			int index = dictGenHashFunction((const unsigned char *)&user_id, sizeof(int)) % able;
+			for (auto nit = node_list.begin(); nit != node_list.end(); nit++)
+			{
+				Node *node = *nit;
+				if (!node->connected)
+				{
 					continue;
 				}
-				if (!index) {
+				if (!index)
+				{
 					//LOGD("user[uid:%s] route to sid:%s according to ROUTE_HASH", user_id.c_str(), sidName(sid));
 					return node;
 				}
@@ -404,55 +457,62 @@ CNode::Node* CNode::getAccessNode(int sid, const std::string& user_id)
 	return NULL;
 }
 
-int CNode::getUserNodeId(int sid,const std::string& user_id)
+int CNode::getUserNodeId(int sid, const std::string &user_id)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
-	Node* node = getAccessNode(sid, user_id);
-	if (node) {
+	Node *node = getAccessNode(sid, user_id);
+	if (node)
+	{
 		return node->nid;
 	}
 	return -1;
 }
 
-int CNode::getUserNodeSock(int sid, const std::string& user_id)
+int CNode::getUserNodeSock(int sid, const std::string &user_id)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
-	Node* node = getAccessNode(sid, user_id);
-	if (node) {
+	Node *node = getAccessNode(sid, user_id);
+	if (node)
+	{
 		return node->fd;
 	}
 	return -1;
 }
 
-int CNode::getUserNodeIdSock(int sid, const std::string& user_id,int& sockfd)
+int CNode::getUserNodeIdSock(int sid, const std::string &user_id, int &sockfd)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
-	Node* node = getAccessNode(sid, user_id);
-	if (node) {
+	Node *node = getAccessNode(sid, user_id);
+	if (node)
+	{
 		sockfd = node->fd;
 		return node->nid;
 	}
 	return -1;
 }
 
-CNode::Node * CNode::addNode(int sid, int nid)
+CNode::Node *CNode::addNode(int sid, int nid)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
 	auto sit = m_services.find(sid);
-	if (sit != m_services.end()) {
-		auto& node_list = sit->second;
-		for (auto nit = node_list.begin(); nit != node_list.end(); nit++) {
-			if ((*nit)->nid == nid) {
+	if (sit != m_services.end())
+	{
+		auto &node_list = sit->second;
+		for (auto nit = node_list.begin(); nit != node_list.end(); nit++)
+		{
+			if ((*nit)->nid == nid)
+			{
 				return *nit;
 			}
 		}
-		Node * node = new Node(sid, nid);
+		Node *node = new Node(sid, nid);
 		node_list.push_back(node);
 		return node;
 	}
-	else {
-		std::list<Node*> node_list;
-		Node * node = new Node(sid, nid);
+	else
+	{
+		std::list<Node *> node_list;
+		Node *node = new Node(sid, nid);
 		node_list.push_back(node);
 		m_services[sid] = node_list;
 		return node;
@@ -463,11 +523,14 @@ int CNode::delNode(int sid, int nid)
 {
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
 	auto sit = m_services.begin();
-	while (sit != m_services.end()) {
-		auto& node_list = sit->second;
-		for (auto nit = node_list.begin(); nit != node_list.end(); nit++) {
-			Node* node = *nit;
-			if (node->sid == sid && node->nid == nid) {
+	while (sit != m_services.end())
+	{
+		auto &node_list = sit->second;
+		for (auto nit = node_list.begin(); nit != node_list.end(); nit++)
+		{
+			Node *node = *nit;
+			if (node->sid == sid && node->nid == nid)
+			{
 				LOGE("del node[sid:%s nid:%d] success", sidName(sid), nid);
 				node->unable = true;
 				//node_list.erase(nit);
@@ -481,7 +544,7 @@ int CNode::delNode(int sid, int nid)
 	return -1;
 }
 
-int CNode::setRouteStragry(Node * node, int stragry, const std::list<std::string>& user_list)
+int CNode::setRouteStragry(Node *node, int stragry, const std::list<std::string> &user_list)
 {
 	node->stragry = stragry;
 	node->user_list.clear();
@@ -493,13 +556,19 @@ int CNode::recordMsgSlot()
 	std::lock_guard<std::recursive_mutex> lock_1(m_services_mutex);
 	memset(msg_slots, 0, sizeof(msg_slots));
 	auto sit = m_services.find(SID_MSG);
-	if (sit != m_services.end()) {
-		auto& node_list = sit->second;
-		for (auto nit = node_list.begin(); nit != node_list.end(); nit++) {
-			if ((*nit)->stragry == ROUTE_SLOT) {
-				for (int i = 0; i<CLUSTER_SLOTS/8; i++) {
-					for (int j = 0; j<8; j++) {
-						if ((*nit)->slots[i] & 1 << j) {
+	if (sit != m_services.end())
+	{
+		auto &node_list = sit->second;
+		for (auto nit = node_list.begin(); nit != node_list.end(); nit++)
+		{
+			if ((*nit)->stragry == ROUTE_SLOT)
+			{
+				for (int i = 0; i < CLUSTER_SLOTS / 8; i++)
+				{
+					for (int j = 0; j < 8; j++)
+					{
+						if ((*nit)->slots[i] & 1 << j)
+						{
 							//printf("%d   ", 8 * i + j);
 							msg_slots[8 * i + j] = *nit;
 						}
